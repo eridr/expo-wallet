0a916c20b8cbcd594441769703778e11748c752d
Erik Dreifaldt <erik.dreifaldt@gmail.com>
2025-08-29 13:01:00 +0200

    refactor: remove unused ExpoWalletView and related functionality from module

--- src/ExpoWallet.types.ts
+++ src/ExpoWallet.types.ts
@@ -1,19 +1,3 @@
-import type { StyleProp, ViewStyle } from 'react-native';
+// place holder for better upcoming fucntions 
+export type ExpoWalletModuleEvents = {};
 
-export type OnLoadEventPayload = {
-  url: string;
-};
-
-export type ExpoWalletModuleEvents = {
-  onChange: (params: ChangeEventPayload) => void;
-};
-
-export type ChangeEventPayload = {
-  value: string;
-};
-
-export type ExpoWalletViewProps = {
-  url: string;
-  onLoad: (event: { nativeEvent: OnLoadEventPayload }) => void;
-  style?: StyleProp<ViewStyle>;
-};
--- src/ExpoWalletModule.web.ts
+++ src/ExpoWalletModule.web.ts
@@ -3,10 +3,6 @@ import { registerWebModule, NativeModule } from 'expo';
 import { ExpoWalletModuleEvents } from './ExpoWallet.types';
 
 class ExpoWalletModule extends NativeModule<ExpoWalletModuleEvents> {
-  PI = Math.PI;
-  async setValueAsync(value: string): Promise<void> {
-    this.emit('onChange', { value });
-  }
   hello() {
     return 'Hello world! 👋';
   }
